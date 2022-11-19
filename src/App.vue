6aa59bdd210435c9b3e5c85f02911fb79c0a3b75
emmet-lv99 <dachanisfull@gmail.com>
2022-11-19 14:53:59 +0900

    ~set eslint/prettier for vscode

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from "./components/HelloWorld.vue"
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
   <div>
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
